3cb53a3683f9f7800db5f0c9945c60acf4102aa4
fujified <uktras@gmail.com>
2021-02-08 00:29:46 +0300

    drag&drop done

--- src/test/java/DragAndDrop.java
+++ src/test/java/DragAndDrop.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
     @Test
     void DragAndDropWithSelenideActions() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(0,150).release().perform();
+//        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(100,0).release().perform();
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 }
