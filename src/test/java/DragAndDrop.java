6dcbe350d10d3d86f61fe7e30e21b1ef4cdcd21d
fujified <uktras@gmail.com>
2021-02-07 23:48:05 +0300

    added drag&drop class

--- src/test/java/DragAndDrop.java
+++ src/test/java/DragAndDrop.java
@@ -1,3 +1,13 @@
-public class DragAndDrop {
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDrop {
+    @Test
+    void DragAndDropWithSelenideActions() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(0,150).release().perform();
+
+        $.sleep(20000);
+    }
 }
