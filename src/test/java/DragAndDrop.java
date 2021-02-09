b25fc76e20915513f771711fb8d4214602a3f7d9
fujified <uktras@gmail.com>
2021-02-09 10:35:14 +0300

    fixed dragAndDropWithSelenideActions

--- src/test/java/DragAndDrop.java
+++ src/test/java/DragAndDrop.java
@@ -5,7 +5,7 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
     @Test
-    void DragAndDropWithSelenideActions() {
+    void dragAndDropWithSelenideActions() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
 //        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(100,0).release().perform();
         $("#column-a").dragAndDropTo($("#column-b"));
